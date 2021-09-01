e9083ca9fb71f89ee7d5da89c97dbc0d79214fe0
Takuto Nanjo <16991946+taku-n@users.noreply.github.com>
2021-09-01 09:18:30 +0900

    a commit

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,9 @@ use warp::Filter;
 
 #[tokio::main]
 async fn main() {
+    std::env::set_var("RUST_LOG", "log");
+    env_logger::init();
+
     // If you listen to an IPv6 socket, you listen to an IPv4 socket too.
     // Test by "curl -6 http://foo.com/" and "curl -4 http://foo.com/".
     // https://stackoverflow.com/questions/45281740/how-to-open-ipv4-and-ipv6-sockets-on-the-same-port-with-http-module-in-nodejs
@@ -22,10 +25,17 @@ async fn main() {
     // /hello and /hello/ get a message.
     //let routes = warp::path("hello").and(warp::path::end()).map(|| "hello, world\n");
 
+    // /hello/world and /hello/world/ get a message.
+    //let routes = warp::path("hello").and(warp::path("world")).and(warp::path::end())
+    //        .map(|| "hello, world\n");
+
     // Respond in HTML.
     //let routes = warp::any().map(|| warp::reply::html(HTML));
 
-    let routes = warp::any().map(|| "hello, world\n").with(warp::trace::request());
+    // Show logs like
+    // [2021-05-09T14:52:23Z INFO  warp::filters::log] [:: (Client Address)]:12345
+    // "GET / HTTP/1.1" 200 "-" "curl/7.76.1" 136.219µs
+    let routes = warp::any().map(|| "hello, world\n").with(warp::log("log"));
 
     warp::serve(routes).run(http).await;
 }
